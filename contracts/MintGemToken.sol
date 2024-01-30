bc20ec813094132fe6f3733db4e16d7f65c86a8a
lala-david <wnswns1946@naver.com>
2024-01-30 17:54:31 +0900

    #1 Minting Test

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -4,20 +4,31 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintGemToken is ERC721Enumerable {
+contract MintGemToken is ERC721Enumerable, Ownable {
+    // nft 발행량 
+    uint constant public MAX_TOKEN_COUNT = 1000;
+    uint constant public TOKEN_RANK_LENGTH = 4;
+    uint constant public TOKEN_TYPE_LENGTH = 4;
+
     string public metadataURI;
 
-    constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) {
+    uint public gemTokenPrice = 0;
+
+    constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) Ownable(msg.sender) {
         metadataURI = _metadataURI;
-    } 
+    }
 
     struct GemTokenData {
         uint gemTokenRank;
         uint gemTokenType;
     }
 
-    mapping(uint => GemTokenData) public gemTokenData;                                                                                                  
+    mapping(uint => GemTokenData) public gemTokenData;       
+
+    // rank, type 테이블
+    uint[TOKEN_RANK_LENGTH][TOKEN_TYPE_LENGTH] public gemTokenCount;                                                                                           
 
     function tokenURI(uint _tokenId) override public view returns(string memory) {
         string memory gemTokenRank = Strings.toString(gemTokenData[_tokenId].gemTokenRank);
@@ -26,12 +37,101 @@ contract MintGemToken is ERC721Enumerable {
         return string(abi.encodePacked(metadataURI, '/', gemTokenRank, '/', gemTokenType, '.json'));
     }
 
-    function mintGemToken() public {
+
+    function mintGemToken() public payable {
+        require(gemTokenPrice <= msg.value, "Not enough Eth.");
+        require(MAX_TOKEN_COUNT > totalSupply(), "No more minting is possible.");
         // 총 nft 발행량 
         uint tokenId = totalSupply() +1;
 
-        gemTokenData[tokenId] = GemTokenData(1, 1);
+        GemTokenData memory randomTokenData = randomGenerator(msg.sender, tokenId);
+
+        gemTokenData[tokenId] = GemTokenData(randomTokenData.gemTokenRank, randomTokenData.gemTokenType);
+
+        gemTokenCount[randomTokenData.gemTokenRank - 1][randomTokenData.gemTokenType -1] +=1;
+
+        payable(owner()).transfer(msg.value);
 
         _mint(msg.sender, tokenId);
     }
+
+    
+    function getGemTokenCount() public view returns(uint[TOKEN_RANK_LENGTH][TOKEN_TYPE_LENGTH] memory) {
+        return gemTokenCount;
+    }
+
+    function getGemTokenRank(uint _tokenId)  public view returns(uint) {
+        return gemTokenData[_tokenId].gemTokenRank;
+    }
+
+    
+    function getGemTokenType(uint _tokenId)  public view returns(uint) {
+        return gemTokenData[_tokenId].gemTokenType;
+    }
+
+
+    function randomGenerator(address _msgSender, uint _tokenId) private view returns(GemTokenData memory) {
+        uint randomNum = uint(keccak256(abi.encodePacked(blockhash(block.timestamp), _msgSender, _tokenId))) % 100;
+
+        GemTokenData memory randomTokenData;
+
+            if (randomNum < 5) {
+                if (randomNum == 1) {
+                    randomTokenData.gemTokenRank = 4;
+                    randomTokenData.gemTokenType = 1;
+                    } else if (randomNum == 2) {
+                        randomTokenData.gemTokenRank = 4;
+                        randomTokenData.gemTokenType = 2;
+                    } else if (randomNum == 3) {
+                        randomTokenData.gemTokenRank = 4;
+                        randomTokenData.gemTokenType = 3;
+                    } else {
+                        randomTokenData.gemTokenRank = 4;
+                        randomTokenData.gemTokenType = 4;
+                    }
+                } else if (randomNum < 13) {
+                    if (randomNum < 7) {
+                        randomTokenData.gemTokenRank = 3;
+                        randomTokenData.gemTokenType = 1;
+                    } else if (randomNum < 9) {
+                        randomTokenData.gemTokenRank = 3;
+                        randomTokenData.gemTokenType = 2;
+                    } else if (randomNum < 11) {
+                        randomTokenData.gemTokenRank = 3;
+                        randomTokenData.gemTokenType = 3;
+                    } else {
+                        randomTokenData.gemTokenRank = 3;
+                        randomTokenData.gemTokenType = 4;
+                    }
+                } else if (randomNum < 37) {
+                    if (randomNum < 19) {
+                        randomTokenData.gemTokenRank = 2;
+                        randomTokenData.gemTokenType = 1;
+                    } else if (randomNum < 25) {
+                        randomTokenData.gemTokenRank = 2;
+                        randomTokenData.gemTokenType = 2;
+                    } else if (randomNum < 31) {
+                        randomTokenData.gemTokenRank = 2;
+                        randomTokenData.gemTokenType = 3;
+                    } else {
+                        randomTokenData.gemTokenRank = 2;
+                        randomTokenData.gemTokenType = 4;
+                    }
+                } else {
+                    if (randomNum < 52) {
+                        randomTokenData.gemTokenRank = 1;
+                        randomTokenData.gemTokenType = 1;
+                    } else if (randomNum < 68) {
+                        randomTokenData.gemTokenRank = 1;
+                        randomTokenData.gemTokenType = 2;
+                    } else if (randomNum < 84) {
+                        randomTokenData.gemTokenRank = 1;
+                        randomTokenData.gemTokenType = 3;
+                    } else {
+                        randomTokenData.gemTokenRank = 1;
+                        randomTokenData.gemTokenType = 4;
+                    }
+            }
+            return randomTokenData;
+        } 
 }
---
+++ contracts/Mynft.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNft is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // 원하는 이름과 심볼을 입력합니다.
+    constructor() ERC721("chicken", "symbol") Ownable(msg.sender) {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
